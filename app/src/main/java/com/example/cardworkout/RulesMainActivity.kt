92ca358d0318fc64bc7a42ab20215ed1fde16e5b
yusufsenturk <yusuf.joe6161@gmail.com>
2024-05-31 17:41:34 +0300

    return button added

--- app/src/main/java/com/example/cardworkout/RulesMainActivity.kt
+++ app/src/main/java/com/example/cardworkout/RulesMainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.cardworkout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cardworkout.databinding.ActivityRulesMainBinding
 
 class RulesMainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRulesMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_rules_main)
+        binding = ActivityRulesMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        goBackClickListener()
+    }
+    private fun goBackClickListener(){
+        binding.button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
